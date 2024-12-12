36e29d021e9cb547ffef80a521870b044e8875ed
yeo99 <qw6375@gmail.com>
2024-12-13 03:38:26 +0900

    Feat: 좌표 정확도 조정

--- models/taxi_reservation.py
+++ models/taxi_reservation.py
@@ -7,11 +7,11 @@ class TaxiReservation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(255), nullable=False)
     starting_point = db.Column(db.String(255), nullable=False)
-    starting_point_latitude = db.Column(db.Float, nullable=False)  # 출발지 위도
-    starting_point_longitude = db.Column(db.Float, nullable=False) # 출발지 경도
+    starting_point_latitude = db.Column(db.Numeric(precision=10, scale=8), nullable=False)  # 출발지 위도
+    starting_point_longitude = db.Column(db.Numeric(precision=11, scale=8), nullable=False) # 출발지 경도
     arrival_point = db.Column(db.String(255), nullable=False)
-    arrival_point_latitude = db.Column(db.Float, nullable=False)   # 도착지 위도
-    arrival_point_longitude = db.Column(db.Float, nullable=False)  # 도착지 경도
+    arrival_point_latitude = db.Column(db.Numeric(precision=10, scale=8), nullable=False)   # 도착지 위도
+    arrival_point_longitude = db.Column(db.Numeric(precision=11, scale=8), nullable=False)  # 도착지 경도
     reservation_phone_number = db.Column(db.String(255), nullable=False)
     reservation_datetime = db.Column(db.DateTime, nullable=True)
     call_type = db.Column(db.String(10), nullable=False)
